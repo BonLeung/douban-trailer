34d04dc0894badaf5f5aba2dde38af0448d98f33
BonLeung <654592397@qq.com>
2018-02-04 17:25:08 +0800

    commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.body = '电影首页'
+  await ctx.render('index', {
+    you: 'Luke',
+    me: 'BonLeung'
+  })
 })
 
 app.listen(9000, () => {
-    console.log('app is running at localhost:9000')
+  console.log('app is running at localhost:9000')
 })
