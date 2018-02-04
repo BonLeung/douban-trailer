6ca986668f5977d77f657f5fa0346210fd10ae39
BonLeung <654592397@qq.com>
2018-02-04 13:19:01 +0800

    add html template

--- server/tpl/html.js
+++ server/tpl/html.js
@@ -3,7 +3,7 @@ module.exports = `
   <html>
     <head>
       <meta charset="utf-8">
-      <meat name="viewport" content="width=device-widt,h initial-scale=1, maximum-scale=1, minimum-scale=1">
+      <meta name="viewport" content="width=device-widt,h initial-scale=1, maximum-scale=1, minimum-scale=1">
       <title>Koa Server HTML</title>
       <link href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
